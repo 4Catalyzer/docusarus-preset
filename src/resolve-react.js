a61b7f65957c8d69132faa93cd6e974f0981322b
Jason Quense <monastic.panic@gmail.com>
2021-07-08 09:23:01 -0400

    fix: resolve runtimes

--- src/resolve-react.js
+++ src/resolve-react.js
@@ -1,10 +1,21 @@
 module.exports = () => ({
   name: 'resolve-react',
   configureWebpack() {
+    let runtimes;
+    try {
+      runtimes = {
+        'react/jsx-runtime': require.resolve('react/jsx-runtime'),
+        'react/jsx-dev-runtime': require.resolve('react/jsx-dev-runtime'),
+      };
+    } catch (e) {
+      runtimes = {};
+    }
+
     return {
       resolve: {
         alias: {
-          'react': require.resolve('react'),
+          'react$': require.resolve('react'),
+          ...runtimes,
           'react-dom$': require.resolve('react-dom'),
           'react-dom/server': require.resolve('react-dom/server'),
         },
